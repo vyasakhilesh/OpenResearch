abc5a7a95599e87b41cfcce3baa44cbc400d4288
vyasakhilesh <akh.vyas@gmail.com>
2026-06-20 09:27:56 +0200

    first commit

--- flows/create_openresearch_events_flow.py
+++ flows/create_openresearch_events_flow.py
@@ -83,6 +83,8 @@ def create_openresearch_events_flow(
                 summary = f"Added upcoming edition for {series} {year} (automated(LLM-assisted) edit)"
                 res = create_page(api_url, page_title, fixed, csrf_token, summary, session, dry_run)
                 logger.info("Create result for page_title %s: result: %s", page_title, res['error']['code'] if res.get('error') else res)
+                if res.get('error'):
+                    logger.error('Create result for page_title %s: result: %s", page_title, res['error']['code']')
                 # time.sleep(1)
             except Exception as e:
                 logger.error("Exception for %s: %s", page_title, e)
