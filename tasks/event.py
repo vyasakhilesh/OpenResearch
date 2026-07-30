bbbb2d035ef111aa8df1997bcbf5406ab0aa1120
vyasakhilesh <akh.vyas@gmail.com>
2026-07-30 08:07:02 +0200

    reconfig pattern

--- tasks/event.py
+++ tasks/event.py
@@ -69,9 +69,11 @@ def fix_event_wikitext(api_url: str, page_titles: List[str], session, csrf_token
         # fix duplicates and clean template using LLM if needed
         try:
             event_wikitext = get_page_wikitext(api_url, page_title, session)
-            pattern = r"=== Sources ===\s*\}\s*"
+            pre_src_pattern = r"=== Sources ===\s*\}}\s*"
+            src_pattern = r"=== Sources ===\s*\}\s*"
             ordinal_re = re.compile(r'\|\s*ordinal\s*=\s*["\']?\s*\d{4}\s*["\']?', re.IGNORECASE)
-            event_wikitext = re.sub(pattern, '', event_wikitext)
+            event_wikitext = re.sub(pre_src_pattern, '', event_wikitext)
+            event_wikitext = re.sub(src_pattern, '', event_wikitext)
             event_wikitext = ordinal_re.sub('', event_wikitext)
             summary = f"Cleaned {page_title} with new text {event_wikitext}"
             res = edit_page(api_url, page_title, event_wikitext, csrf_token, session, summary, dry_run)
